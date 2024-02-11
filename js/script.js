9c431204b672fe463aac6654048109f70870bfee
gegabayte <xasanboymuhammaddinov41@gmail.com>
2024-02-11 16:13:12 +0500

    added

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const arr = () => {
+    document.write('Qalesan Ibrohim nima gap bolyaptimi oqishlar')
+}
+const arr1 = () => {
+    document.write(`Qalesan Aziz nima gap bolyaptimi oqishlar`)
+
+}
+const arr2 = () => {
+    document.write(`Qalesan Omad nima gap bolyaptimi oqishlar`)
+}
+const arr3 = () => {
+    document.write(`Qalesan Zikrullo nima gap bolyaptimi oqishlar`)
+}
+const arr4 = () => {
+    document.write(`Qalesan Asad nima gap bolyaptimi oqishlar`)
+}
+const arr5 = () => {
+    document.write(`Qalesan Keldibek nima gap bolyaptimi oqishlar`)
+}
+const arr6 = () => {
+    document.write(`Qalesan Jasur nima gap bolyaptimi oqishlar`)
+}
